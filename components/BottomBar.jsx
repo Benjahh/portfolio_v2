5d93dbddd1b5707339ac450ff2211287a778fe8f
Benjahh <lucasbenja09@hotmail.com>
2024-01-07 23:52:22 -0300

    styling social link and copyright components

--- components/BottomBar.jsx
+++ components/BottomBar.jsx
@@ -6,13 +6,12 @@ const BottomBar = () => {
   const variant = {
     hidden: {
       opacity: 0,
+      y: '200%',
       textColor: 'rgba(255, 255, 255)',
     },
     visible: {
       opacity: 1,
-      transition: {
-        staggerChildren: 0.025,
-      },
+      y: 0,
     },
   };
 
@@ -22,7 +21,7 @@ const BottomBar = () => {
       animate="visible"
       variants={variant}
       transition={{ duration: 0.5, staggerChildren: 0.025 }}
-      className="text-xs font-bold z-40 mr-4 text-white bottom-0 right-0 absolute self-end flex flex-col"
+      className="text-sm font-bold z-40  mr-4 mb-4 text-white bottom-0 right-0 absolute self-end flex flex-col"
     >
       <p className="">2023 - PRESENT © Lucas Agüero</p>
       <p className="">
--- components/SocialLinks.jsx
+++ components/SocialLinks.jsx
@@ -1,9 +1,57 @@
+'use client';
+
+import { FiGithub, FiLinkedin } from 'react-icons/fi';
+import { motion } from 'framer-motion';
+
 const SocialLinks = () => {
+  const variant = {
+    hidden: {
+      opacity: 0,
+    },
+    visible: {
+      opacity: 1,
+      transition: {
+        delayChildren: 0.5,
+        staggerChildren: 1,
+      },
+    },
+  };
+
+  const childrenVariant = {
+    hidden: {
+      x: '200%',
+    },
+    visible: {
+      x: 0,
+    },
+  };
   return (
-    <div className="text-red-400 border w-full h-20">
-      <p>2023 - Present © Lucas Agüero</p>
-      <p>Created with NextJS</p>
-    </div>
+    <motion.div
+      initial="hidden"
+      animate="visible"
+      variants={variant}
+      className="text-red-400 absolute right-0 mr-20 mt-20 flex flex-row gap-4 top-0 h-20"
+    >
+      <motion.a
+        initial="hidden"
+        animate="visible"
+        variants={childrenVariant}
+        href="https://www.mudai.dev"
+        onHover={{ scale: 2 }}
+        className="hover:cursor-pointer"
+      >
+        <FiGithub className="text-accent" size="40" />
+      </motion.a>
+      <motion.a
+        initial="hidden"
+        animate="visible"
+        variants={variant}
+        href="https://www.mudai.dev"
+        className="hover:cursor-pointer"
+      >
+        <FiLinkedin className="text-white" size="40" />
+      </motion.a>
+    </motion.div>
   );
 };
 
